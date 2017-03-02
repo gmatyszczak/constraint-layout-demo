a094a2c5287a1a3d0f953788329361bb6c9c25ac
Grzegorz Matyszczak <grzegorz.matyszczak@droidsonroids.pl>
2017-03-02 10:18:01 +0100

    initial commit

---
+++ app/src/main/java/pl/droidsonroids/constraintlayoutdemo/MainActivity.java
@@ -0,0 +1,14 @@
+package pl.droidsonroids.constraintlayoutdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
