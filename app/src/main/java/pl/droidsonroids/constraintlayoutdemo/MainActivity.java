1d888ccb78134ea74a84404c2b69749a0e4f7904
Grzegorz Matyszczak <grzegorz.matyszczak@droidsonroids.pl>
2017-04-03 20:06:31 +0200

    add butterknife binding

--- app/src/main/java/pl/droidsonroids/constraintlayoutdemo/MainActivity.java
+++ app/src/main/java/pl/droidsonroids/constraintlayoutdemo/MainActivity.java
@@ -1,14 +1,19 @@
 package pl.droidsonroids.constraintlayoutdemo;
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.constraintLayout) ConstraintLayout mConstraintLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
-
 }
